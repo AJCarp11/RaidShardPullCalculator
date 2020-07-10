d30a631413412252be1c445dca3f4507a82bda95
Andy <Andy@DESKTOP-3J8QAT2.attlocal.net>
2020-07-10 01:39:01 -0400

    Resolved all probabilities not equaling 1 and begun sacred lego pulls.

--- RaidShardPullsCalc/src/RunCalc.java
+++ RaidShardPullsCalc/src/RunCalc.java
@@ -8,13 +8,7 @@ public class RunCalc {
 		GetNumShards info = new GetNumShards();
 		SetRates rates = new SetRates();
 		
-		
 		runAllCalculations(info.getNumAncientShards(), info.getNumVoidShards(), info.getNumSacredShards(), rates.getAncientRates()[0], rates.getAncientRates()[1], rates.getAncientRates()[2], rates.getVoidRates()[0], rates.getVoidRates()[1], rates.getVoidRates()[2], rates.getSacredRates()[0], rates.getSacredRates()[1]);
-		
-		
-		//System.out.println(info.getNumAncientShards());
-		
-		//System.out.println(1 + rates.getAncientRates()[1]);
 	
 	}
 	
@@ -22,6 +16,7 @@ public class RunCalc {
 		
 		getNumLegosFromAncients(numAncients, ancientPullRateCommon, ancientPullRateEpic, ancientPullRateLego);
 		getNumLegosFromVoids(numVoids, voidPullRateCommon, voidPullRateEpic, voidPullRateLego);
+		//getNumLegosFromSacreds(numSacreds, sacredPullRateEpic, sacredPullRateLego);
 		
 	}
 	
@@ -30,18 +25,25 @@ public class RunCalc {
 		double[] percentages = new double[numShards];
 		int count = 5;
 		
-		percentages[0] = (1.0 - Math.pow((1.0-(legoPullRate/100.0)), numShards)) * Math.pow(10, 2);
+		percentages[0] = Math.pow((1.0-(legoPullRate/100.0)), numShards) * Math.pow(10, 2);
+		
+		//double temp1 = percentages[0];
 		
 		for(int i=1; i<numShards; i++) {
 			
-			percentages[i] = (Math.pow((1.0-(legoPullRate/100.0)), (numShards-i))) * (1.0 * Math.pow((legoPullRate/100.0),i)) * Math.pow(10,4); 
+			percentages[i] = numShards * (Math.pow((1.0 - legoPullRate/100.0), numShards - i)) * (Math.pow((legoPullRate/100.0), i)) * Math.pow(10, 2);
+			//temp1 = temp1 + percentages[i];
 		}
+		
+		
 		
 		System.out.println();
 		System.out.println("Ancient Shard Results:");
 		for (int i=0; i<count-1;i++) {
 			System.out.println("Your chances of pulling " + i + " legendaries are " + percentages[i] + "%");
 		}
+		
+		//System.out.println("Temp: " + temp1);
 		
 	}
 	
@@ -50,19 +52,49 @@ public class RunCalc {
 		double[] percentages = new double[numShards];
 		int count = 5;
 		
-		percentages[0] = (1.0 - Math.pow((1.0-(legoPullRate/100.0)), numShards)) * Math.pow(10, 2);
+		percentages[0] = Math.pow((1.0-(legoPullRate/100.0)), numShards) * Math.pow(10, 2);
 		
+		//double temp2 = percentages[0];
 		for(int i=1; i<numShards; i++) {
 			
-			percentages[i] = (Math.pow((1.0-(legoPullRate/100.0)), (numShards-i))) * (1.0 * Math.pow((legoPullRate/100.0),i)) * Math.pow(10,4); 
+			
+			percentages[i] = numShards * (Math.pow((1.0 - legoPullRate/100.0), numShards - i)) * (Math.pow((legoPullRate/100.0), i)) * Math.pow(10, 2);
+			//temp2 = temp2 + percentages[i];
 		}
+		
+
 		
 		System.out.println();
 		System.out.println("Void Shard Results:");
 		for (int i=0; i<count-1;i++) {
 			System.out.println("Your chances of pulling " + i + " legendaries are " + percentages[i] + "%");
 		}
+	
+		//System.out.println("Temp: " + temp2);
+	}
+	
 		
+		public static void getNumLegosFromSacreds(int numShards, double epicPullRate, double legoPullRate) {
+			
+			double[] percentages = new double[numShards];
+			int count = 5;
+			
+			percentages[0] = Math.pow((epicPullRate/100.0), numShards) * Math.pow(10, 2);
+			
+			//double temp = percentages[0];
+			
+			for(int i=1; i<numShards; i++) {
+				
+				percentages[i] = Math.pow((epicPullRate/100.0), (numShards-i)) * Math.pow((legoPullRate/100.0),i) * Math.pow(10,2); 
+				//temp = temp + percentages[i];
+			}
+			
+			System.out.println();
+			System.out.println("Sacred Shard Results:");
+			for (int i=0; i<count-1;i++) {
+				System.out.println("Your chances of pulling " + i + " legendaries are " + percentages[i] + "%");
+			}
+		//System.out.println("Temp: " + temp);
 	}
 	
 	
--- RaidShardPullsCalc/src/SetRates.java
+++ RaidShardPullsCalc/src/SetRates.java
@@ -5,12 +5,10 @@ public class SetRates {
 	// This is where you can change pull rates for x2 summons!!
 	static double[] AncientPullRates = {91.5, 8.0, 0.5};
 	static double[] VoidPullRates = {83.0, 16.0, 1.0};
-	static double[] SacredPullRates = {94, 6.0};
+	static double[] SacredPullRates = {94.0, 6.0};
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				
-		SetRates userRates = new SetRates();
 		
 		getAncientRates();
 		getVoidRates();
